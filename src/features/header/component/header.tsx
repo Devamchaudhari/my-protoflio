1c448256576d33f953f8f521a3e289476e2998b4
Chaudhari Devam <devamchaudhari40@gmail.com>
2022-12-18 17:06:45 +0530

    added mail link / added social medias link in social

--- src/features/header/component/header.tsx
+++ src/features/header/component/header.tsx
@@ -3,20 +3,22 @@ import ProfileImage from '../../../assets/images/Profile.png';
 import { RightArrow } from '../../../shared/icons/icons';
 import '../header.scss';
 const Header = () => {
-    return (
-        <div className="full-width d-flex justify-content-space-between align-items-center">
-            <div className="profile  position--absolute" />
-            <div className="profile-image-wrapper  position--relative">
-                <img src={ProfileImage} alt="profile" className="width--100" />
-            </div>
-            <div>
-                <button className="d-flex align-items-center p--10 mr-20 connect-btn">
-                    Let's connect
-                    <RightArrow />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="full-width d-flex justify-content-space-between align-items-center">
+      <div className="profile  position--absolute" />
+      <div className="profile-image-wrapper  position--relative">
+        <img src={ProfileImage} alt="profile" className="width--100" />
+      </div>
+      <div>
+        <a href="mailto:devamchaudhari40@gmail.com">
+          <button className="d-flex align-items-center p--10 mr-20 connect-btn">
+            Let's connect
+            <RightArrow />
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/features/myData/component/social.tsx
+++ src/features/myData/component/social.tsx
@@ -20,31 +20,49 @@ const Social = () => {
         <p className="intro-title mt--30">Social</p>
         <div className="d-flex flex-wrap mt--30 gap-15 social-icons-container">
           <div className="mr--20">
-            <img src={stakeOverflowImage} alt="javascript" />
+            <a href="https://stackoverflow.com/users/17909143/chaudhari-devam">
+              <img src={stakeOverflowImage} alt="javascript" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={github} alt="typescript" />
+            <a href="https://github.com/Devamchaudhari">
+              <img src={github} alt="typescript" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={devImage} alt="reactJs" />
+            <a href="https://dev.to/chaudharidevam">
+              <img src={devImage} alt="reactJs" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={codePenImage} alt="html5" />
+            <a href="https://codepen.io/your-work">
+              <img src={codePenImage} alt="html5" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={dribbleImage} alt="css3" />
+            <a href="https://dribbble.com/ChaudhariDevam">
+              <img src={dribbleImage} alt="css3" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={behanceImage} alt="sass" />
+            <a href="https://www.behance.net/devamchaud7cf7">
+              <img src={behanceImage} alt="sass" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={linkedInImage} alt="bootstrap" />
+            <a href="https://www.linkedin.com/in/devam-chaudhari-537ba7134/">
+              <img src={linkedInImage} alt="bootstrap" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={mediumImage} alt="webpack" />
+            <a href="https://medium.com/@devamchaudhari40">
+              <img src={mediumImage} alt="webpack" />
+            </a>
           </div>
           <div className="mr--20">
-            <img src={twitterImage} alt="nodejs" />
+            <a href="https://twitter.com/ChaudhariDevam">
+              <img src={twitterImage} alt="nodejs" />
+            </a>
           </div>
         </div>
       </div>
